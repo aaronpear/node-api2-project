2991eb673b64d8196d51f744b70f5456df9018d4
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-01-11 15:30:03 -0800

    first two GET functions done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,6 +3,30 @@ const express = require('express');
 const router = express.Router();
 const Post = require('./posts-model.js');
 
+router.get('/', (req, res) => {
+    Post.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "The posts information could not be retrieved" });
+        })
+})
 
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            if (!post) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            } else {
+                res.status(200).json(post);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "The post information could not be retrieved" });
+        })
+})
 
 module.exports = router;
