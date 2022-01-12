5b5b44bb62054fe12a10e3317e8e8afd7ad2f654
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-01-11 16:28:30 -0800

    POST and PUT mostly done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -29,4 +29,41 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    const { title, contents } = req.body;
+
+    if (!title || !contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" });
+    } else {
+        Post.insert({ title, contents })
+            .then(post => {
+                res.status(201).json(post);
+            })
+            .catch(err => {
+                res.status(500).json({ message: "There was an error while saving the post to the database" });
+            })
+    }
+})
+
+router.put('/:id', (req, res) => {
+    const { title, contents } = req.body;
+    const { id } = req.params;
+
+    if (!title || !contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" });
+    } else {
+        Post.update(id, { title, contents })
+            .then(post => {
+                if (!post) {
+                    res.status(404).json({ message: "The post with the specified ID does not exist" });
+                } else {
+                    res.status(200).json(post);
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ message: "The post information could not be modified" });
+            })
+    }
+})
+
 module.exports = router;
